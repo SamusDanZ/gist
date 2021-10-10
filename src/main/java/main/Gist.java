083d0159f8fb6b4ec6cd924b7bc866dd20ba3234
Daniel Muñoz <84843032+SamusDanZ@users.noreply.github.com>
2021-10-10 23:00:23 +0200

    Update Gist.java

--- src/main/java/main/Gist.java
+++ src/main/java/main/Gist.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Gist {
     public static void main(String[] args) {
+        System.out.println("Hola Jimena, te he tocado el codigo XD");
         Scanner sc = new Scanner(System.in);
     }
 }
